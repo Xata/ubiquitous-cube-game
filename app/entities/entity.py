bf93e9dbf7906756f545f024e11bb08b9f77555a
Maciek <1977132+Xata@users.noreply.github.com>
2024-04-22 13:46:06 -0600

    Update entity.py
    Added a line to keep track of an entity's name in the Entity class.

--- app/entities/entity.py
+++ app/entities/entity.py
@@ -2,6 +2,7 @@
 # Entities that the player can interact with
 
 class Entity:
-    def __init__(self, id):
+    def __init__(self, id, name):
         self.id = id
+        self.entity_name = name
         
